0804202a5a9cf0b40f7061e62aab7bc23f6c333c
kyungmin0729 <kyungmin0729@naver.com>
2023-11-10 14:31:38 +0900

    setting 설정 중..

--- src/main/java/org/koreait/configs/SecurityConfig.java
+++ src/main/java/org/koreait/configs/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
     }
