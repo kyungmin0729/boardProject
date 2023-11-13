965c4f38f410922ed8e9135f812c76360e6b4255
kyungmin0729 <kyungmin0729@naver.com>
2023-11-13 15:51:13 +0900

    security 설정중...

--- src/main/java/org/koreait/models/member/MemberInfo.java
+++ src/main/java/org/koreait/models/member/MemberInfo.java
@@ -6,25 +6,32 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import org.koreait.entities.Member;
 
 @Data
 @Builder
 public class MemberInfo implements UserDetails {
 
+    private String email;
+    private String password;
+
+    private Member member;
+
+    private Collection<? extends GrantedAuthority> authorities;
     // 권한 기능
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     // Account 관리
--- src/main/java/org/koreait/models/member/MemberInfoService.java
+++ src/main/java/org/koreait/models/member/MemberInfoService.java
@@ -1,14 +1,28 @@
 package org.koreait.models.member;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.entities.Member;
+import org.koreait.repositories.MemberRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberInfoService implements UserDetailsService {
+
+    private final MemberRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Member member = repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return MemberInfo.builder()
+                .email(member.getEmail())
+                .password(member.getPassword()) //hash되어 있는 password
+                .member(member)
+                .build();
     }
 }
