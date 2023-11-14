11467a27570631b308582a7ad6f9b0171f4b3f69
kyungmin0729 <kyungmin0729@naver.com>
2023-11-14 16:30:33 +0900

    security 설정중...

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -22,6 +22,10 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
                 .setViewName("front/main/index");
+
+        registry.addViewController("/mypage").setViewName("front/main/index");
+
+        registry.addViewController("/admin").setViewName("front/main/index");
     }
 
     @Autowired
--- src/main/java/org/koreait/configs/SecurityConfig.java
+++ src/main/java/org/koreait/configs/SecurityConfig.java
@@ -1,5 +1,8 @@
 package org.koreait.configs;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.koreait.models.member.LoginFailureHandler;
 import org.koreait.models.member.LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +12,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
 
 @Configuration
 @EnableConfigurationProperties(FileUploadConfig.class)
@@ -20,6 +27,7 @@ public class SecurityConfig {
 
     @Autowired
     private FileUploadConfig fileUploadConfig;
+    /* 인증 설정 - 로그인 S */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -37,10 +45,33 @@ public class SecurityConfig {
            c.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
                    .logoutSuccessUrl("/member/login");
         });
+        /* 인증 설정 - 로그인 E */
+
 
         http.headers(c -> {
             c.frameOptions(o -> o.sameOrigin());
         });
+
+        /* 인가 설정 - 접근 통제 S */
+        http.authorizeHttpRequests(c -> {
+            c.requestMatchers("/mypage/**").authenticated() // 회원 전용(로그인한 회원만 접근가능)
+                    .requestMatchers("/admin/**").hasAuthority("ADMIN") // 관리자 권한만 접근 가능
+                    .anyRequest().permitAll(); // 나머지 페이지는 권한 필요 x
+        });
+
+        http.exceptionHandling(c -> {
+            c.authenticationEntryPoint((req, resp, e) -> {
+                String URI = req.getRequestURI();
+                if (URI.indexOf("/admin") != -1) { // 관리자 페이지 - 401 응답 코드
+                    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "NOT AUTHORIZED");
+                } else { // 회원전용 페이지(예 - /mypage ) -> 로그인 페이지 이동
+                        String url = req.getContextPath() + "/member/login";
+                                resp.sendRedirect(url);
+                }
+            });
+        });
+
+        /* 인가 설정 - 접근 통제 E */
 
         return http.build();
 
