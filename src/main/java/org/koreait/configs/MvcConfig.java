6ee2a57ee1877f8d6ca01f751b810dbc7c99337a
kyungmin0729 <kyungmin0729@naver.com>
2023-11-20 17:08:22 +0900

    게시판 설정 중 ...

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -7,46 +7,55 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ResourceBundle;
-
 @Configuration
+@EnableJpaAuditing
+@EnableScheduling
 @EnableConfigurationProperties(FileUploadConfig.class)
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileUploadConfig fileUploadConfig;
+
+    @Autowired
+    private CommonInterceptor commonInterceptor;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
                 .setViewName("front/main/index");
 
-        registry.addViewController("/mypage").setViewName("front/main/index");
+        registry.addViewController("/mypage")
+                .setViewName("front/main/index");
 
-        registry.addViewController("/admin").setViewName("front/main/index");
+        registry.addViewController("/admin")
+                .setViewName("front/main/index");
     }
 
-    @Autowired
-    private CommonInterceptor commonInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(commonInterceptor)
                 .addPathPatterns("/**");
-    }
 
-    @Autowired
-    private FileUploadConfig fileUploadConfig;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
         registry.addResourceHandler(fileUploadConfig.getUrl() + "**")
                 .addResourceLocations("file:///" + fileUploadConfig.getPath());
     }
 
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+
         return new HiddenHttpMethodFilter();
     }
 
@@ -58,4 +67,5 @@ public class MvcConfig implements WebMvcConfigurer {
 
         return ms;
     }
+
 }
